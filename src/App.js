1b6a51545d700ee74d7b244a2ba339a7ad62ca18
junseo <qkrwnstj0413@naver.com>
2020-06-16 17:54:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Hello from './hello';
+import World from './World';
+import PassVail from './PassVali';
+import ScrollBox from './ScrollBox';
+import LifeCycleSample from './LifeCycle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+  render() {
+    const buttonColor = {
+      background: "#BBB8DC",
+      border: 0,
+      outline: 0
+    }
+    const {
+      handleClick
+    } = this;
+    const { color } = this.state;
+    return (
 
+      <div className="App">
+        <button onClick={handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={color} />
+        <div>
+          <ScrollBox ref={(ref) => this.scrollBox = ref} />
+          <br />
+          <button style={buttonColor} onClick={() => this.scrollBox.scrollToBottom()}>
+            맨밑으로
+      </button>
+        </div>
+        <Hello name="박준서" />
+        <World />
+        <PassVail />
+      </div>
+    );
+  }
+}
 export default App;
